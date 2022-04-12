f47cd21ee148875bfde9d1c76c492f9dc2dbd2d8
Keith Moravec <kjemdg@yahoo.com>
2022-04-11 19:08:43 -0500

    Tuesday_Start_Group_Session

--- app.py
+++ app.py
@@ -21,14 +21,26 @@ def mentalhealth():
 def resources():
     return render_template("assessment.html")
 
-@app.route('/predict', methods = ['POST'])
+# prediction function
+def ValuePredictor(to_predict_list):
+    to_predict = np.array(to_predict_list).reshape(1,2)
+    loaded_model = pickle.load(open("model.pkl","rb")) # load the model
+    result = loaded_model.predict(to_predict) # predict the values using loded model
+    return result[0]
+
+@app.route('/predict.html', methods = ['POST'])
 def predict():
     int_features = [x for x in request.form.values()]
+
+    l = len(int_features)
+    print(l)
+
     final = np.array(int_features)
     data_unseen = pd.DataFrame([final], columns = cols)
     prediction = predict_model(model, data=data_unseen, round = 0)
     prediction = int(prediction.label[0])
-    return render_template('home.html', pred = 'Expected Diagnosis: {}'.format(prediction))
+    return render_template('/predict.html', pred = 'Expected Diagnosis: {}'.format(prediction))
+    # return render_template('predict.html',prediction)
 
 if __name__ == "__main__":
     app.run()
